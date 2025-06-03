926e5405fad4f8708d92eba9e5f796a6b3f68d5e
Dmitry Matveyev <dmitry.matveyev.job@gmail.com>
2025-06-03 08:50:44 +0200

    polish api code

--- src/back/WebApi/Configuration/FirebaseInitializer.cs
+++ src/back/WebApi/Configuration/FirebaseInitializer.cs
@@ -7,11 +7,8 @@ namespace EverPal.WebApi.Configuration
     {
         public static void Initialize(IConfiguration configuration)
         {
-            // Check if the Firebase Admin SDK has already been initialized
             if (FirebaseApp.DefaultInstance == null)
             {
-                // This expects the GOOGLE_APPLICATION_CREDENTIALS environment variable 
-                // to be set to the path of your service account JSON file
                 FirebaseApp.Create(new AppOptions
                 {
                     Credential = GoogleCredential.GetApplicationDefault()
