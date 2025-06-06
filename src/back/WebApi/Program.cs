bc22a20fbbb9bbc763ab2c11873910bc6f075b46
Dmitry Matveyev <dmitry.matveyev.job@gmail.com>
2025-06-06 07:41:19 +0200

    add auth to Swagger UI

--- src/back/WebApi/Program.cs
+++ src/back/WebApi/Program.cs
@@ -3,12 +3,62 @@ using EverPal.WebApi.Middlewares;
 using EverPal.WebApi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "EverPal API", Version = "v1" });
+    
+    // Add Firebase JWT Authentication
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer {token}'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    // Add Anonymous Authentication
+    options.AddSecurityDefinition("Anonymous", new OpenApiSecurityScheme
+    {
+        Description = "Anonymous Authorization header. Enter 'Anonymous {token}'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Anonymous"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        },
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Anonymous"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Add HttpClient for Firebase Auth API
 builder.Services.AddHttpClient<IFirebaseAuthService, FirebaseAuthService>();
